04f39f6f27eb10ed7dc3ba6116e408761c4d87dd
Richard Bormann <Richard.Bormann@ipa.fraunhofer.de>
2015-09-15 14:21:26 +0200

    added trashbins to new maps

--- ipa_room_segmentation/ros/src/evaluation.cpp
+++ ipa_room_segmentation/ros/src/evaluation.cpp
@@ -536,7 +536,7 @@ int main(int argc, char **argv) {
 			ac.sendGoal(goal);
 
 			//wait for the action to return
-			bool finished_before_timeout = ac.waitForResult(ros::Duration(1200.0));
+			bool finished_before_timeout = ac.waitForResult();
 
 			if (!finished_before_timeout)
 				continue;
