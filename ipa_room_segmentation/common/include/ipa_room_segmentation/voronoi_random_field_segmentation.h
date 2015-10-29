7a17994d6eaddcdc17b1069432307c0f2eb5c50d
Florian Jordan <Florian.jordan@ipa.fraunhofer.de>
2015-10-29 15:02:46 +0100

    added voronoi random field segmentation

---
+++ ipa_room_segmentation/common/include/ipa_room_segmentation/voronoi_random_field_segmentation.h
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+#include <math.h>
+
+#include <dlib/optimization.h>
+
+#include <ctime>
+
+class VoronoiRandomFieldSegmentation
+{
+protected:
+
+public:
+
+	VoronoiRandomFieldSegmentation(); //constructor
+};
