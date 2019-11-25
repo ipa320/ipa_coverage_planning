1e2411f8041d12e85e01789c214734f6e4d15bf8
Max Beutelspacher <max.beutelspacher@ipa.fraunhofer.de>
2019-11-25 15:10:54 +0100

    use tabs instead of spaces

--- ipa_room_segmentation/common/src/cv_boost_loader.cpp
+++ ipa_room_segmentation/common/src/cv_boost_loader.cpp
@@ -3,15 +3,15 @@
 #if CV_MAJOR_VERSION == 2
 void loadBoost(CvBoost& boost, std::string const& filename)
 {
-  boost.load(filename.c_str());
+	boost.load(filename.c_str());
 }
 #else
 void loadBoost(cv::Ptr<cv::ml::Boost> boost, std::string const& filename)
 {
 #if CV_MAJOR_VERSION == 3 && CV_MINOR_VERSION<=2
-  boost->load<cv::ml::Boost>(filename.c_str());
+	boost->load<cv::ml::Boost>(filename.c_str());
 #else
-  boost->load(filename.c_str());
+	boost->load(filename.c_str());
 #endif
 }
 #endif
